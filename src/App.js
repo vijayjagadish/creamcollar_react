3bd8a1d5223600ea756c75f191f2db58d095744b
vijayjagadish <vijayj@creamcollar.com>
2023-11-15 13:35:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Index from './Pages/Index';
+import About_us from './Pages/About_us';
+import Contact_us from './Pages/Contact_us';
+import Login from './Pages/Login';
+import Logout from './Pages/Logout';
+import ForgotPassword from './Pages/ForgotPassword';
+import Dashboard from './Pages/Dashboard';
+import Register from './Pages/Register';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Index/>}/>
+          <Route path='/about_us' element={<About_us/>}/>
+          <Route path='/contact_us' element={<Contact_us/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='logout' element={<Logout/>}/>
+          <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
